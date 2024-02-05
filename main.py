db2ac4f4299c719b6e6113d8b8060947a12dd26e
Prakhar Pratyush <prakhar.pratyush8010@gmail.com>
2024-02-05 23:25:37 +0530

    filepath

--- main.py
+++ main.py
@@ -11,40 +11,45 @@ async def root():
 
 
 # get texts from pdf
+# get texts from pdf
 @app.get("/pdf/{filename}")
 async def read_pdf(filename: str):
-    reader = PdfReader(filename)
-    number_of_pages = len(reader.pages)
-    text = ''
-    for i in range(number_of_pages):
-        page = reader.pages[i]
-        text += page.extract_text()
+    with open(filename, "rb") as file:
+        reader = PdfReader(file)
+        number_of_pages = len(reader.pages)
+        text = ''
+        for i in range(number_of_pages):
+            page = reader.pages[i]
+            text += page.extract_text()
 
     return text
 
 
 @app.get("/pdf/metadata/{filename}")
 async def metadata_pdf(filename: str):
-    reader = PdfReader(filename)
-    meta = reader.metadata
-    metaFile = {"author": meta.author, "title": meta.title, "subject": meta.subject, "creator": meta.creator,
-                " producer": meta.producer, "creation_date": meta.creation_date,
-                "modification_date": meta.modification_date}
+    with open(filename, "rb") as file:
+        reader = PdfReader(file)
+        meta = reader.metadata
+        metaFile = {"author": meta.author, "title": meta.title, "subject": meta.subject, "creator": meta.creator,
+                    "producer": meta.producer, "creation_date": meta.creation_date,
+                    "modification_date": meta.modification_date}
 
     return metaFile
 
 
+
 @app.get("/pdf/img/{filename}")
 async def getImages(filename: str):
-    reader = PdfReader(filename)
-    number_of_pages = len(reader.pages)
-    count = 0
-    for i in range(number_of_pages):
-        page = reader.pages[i]
-        for image_object in page.images:
-            with open(str(count) + image_object.name, "wb") as fp:
-                fp.write(image_object.data)
-                count += 1
+    with open(filename, "rb") as file:
+        reader = PdfReader(file)
+        number_of_pages = len(reader.pages)
+        count = 0
+        for i in range(number_of_pages):
+            page = reader.pages[i]
+            for image_object in page.images:
+                with open(str(count) + image_object.name, "wb") as fp:
+                    fp.write(image_object.data)
+                    count += 1
 
 
 @app.post("/uploadfile")
